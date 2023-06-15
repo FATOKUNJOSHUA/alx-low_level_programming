9bee076cc902344da5ad5a34bbf64e52e2b8d267
FATOKUNJOSHUA <110335529+FATOKUNJOSHUA@users.noreply.github.com>
2023-06-15 22:39:27 +0100

    Update 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,6 @@ printf("size of an int: %lu bytes(s)\n",  (unsigned long)sizeof(b));
 printf("size of a long int: %lu bytes(s)\n",  (unsigned long)sizeof(c));
 printf("size of a long long int: %lu bytes(s)\n",  (unsigned long)sizeof(d));
 printf("size of a float: %lu bytes(s)\n",  (unsigned long)sizeof(f));
+fprintf(stderr, "[Anything]");
 return (0);
-}
-int error() {
-    fprintf(stderr, "[Anything]");
-    return (0);
+{
