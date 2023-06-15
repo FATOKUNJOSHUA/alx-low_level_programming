4d3df24506af0cdcfb1d5cecf62f4edcbba9a45b
Fatokun Joshua <fatokunjoshua0@gmail.com>
2023-06-15 10:47:18 -0700

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - A programe that prints the size of various types on the compter it is compiling
- * Return 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -10,7 +11,6 @@ int b;
 long int c;
 long long int d;
 float f;
-
 printf("size of a char: %lu bytes(s)\n",  (unsigned long)sizeof(a));
 printf("size of an int: %lu bytes(s)\n",  (unsigned long)sizeof(b));
 printf("size of a long int: %lu bytes(s)\n",  (unsigned long)sizeof(c));
