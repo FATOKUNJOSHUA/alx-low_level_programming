d1ef138c4cc96d64d20adf0bfbe178e454595b84
Fatokun Joshua <fatokunjoshua0@gmail.com>
2023-06-17 05:45:35 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@ int main(void)
 		for (d2 = d1 + 1; d2 <= 9; d2++)
 		{
 			{
-				putchar(d1 + '0');
-				putchar(d2 + '0');
+				putchar(d1);
+				putchar(d2);
 				if (!(d1 == 8 && d2 == 9))
 					putchar(',');
 			}
