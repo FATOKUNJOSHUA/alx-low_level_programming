2631011f1a1471e464a5d47afa8b74c719dbabd3
Fatokun Joshua <fatokunjoshua0@gmail.com>
2023-07-18 05:11:19 -0700

    corrections

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,6 +9,7 @@
 	int _strlen(char *s)
 	{
 	int i;
+
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -25,9 +26,10 @@
 *
 * Return: the pointer to dest
 */
-        char *_strcpy(char *dest, char *src)
+	char *_strcpy(char *dest, char *src)
 	{
 	int len, i;
+
 	len = 0;
 	while (src[len] != '\0')
 	{
@@ -52,6 +54,7 @@
 	{
 	dog_t *dog;
 	int len1, len2;
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
